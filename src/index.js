c892367973cc89102e060e1103a86711b4830b2e
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-22 15:30:00 -0700

    installed libraries

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix';
+
+import axios from 'axios';
+// import templateFunction from './templates/oneCardTemplate.hbs';
+// document.body.innerHTML = templateFunction();
