af60b051a54b4a23f229c70d1fc5ab71fcdc8217
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-22 20:16:43 -0700

    dsd

--- src/index.js
+++ src/index.js
@@ -1,66 +1,67 @@
 //*   IMPORTS  libraries
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Notify } from 'notiflix';
 
 import axios from 'axios';
 
-
 //*   IMPORTS  components  files
 // import templateFunction from './templates/oneCardTemplate.hbs';
 // document.body.innerHTML = templateFunction();
 
-import "./css/common.css";
-// import NewsApiService from './js/news-service';
-// import LoadMoreBtn from './js/load-more-btn';
+import './css/common.css';
+import NewsApiService from './js/news-service';
+import LoadMoreBtn from './js/load-more-btn';
 
 //*     CONSTANTS
-// const refs = {
-//   searchForm: document.querySelector('.search-form'),
-//   searchInput: document.querySelector('[type="text"]'),
-//   btn: document.querySelector('[type="submit"]'),
-// };
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  searchInput: document.querySelector('[type="text"]'),
+  submitBtn: document.querySelector('[type="submit"]'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  spinner: document.querySelector('.spinner-border'),
+};
 
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const newsApiService = new NewsApiService();
 
-// const loadMoreBtn = new LoadMoreBtn({
-//   selector: '[data-action="load-more"]',
-//   hidden: true,
-// });
-// const newsApiService = new NewsApiService();
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
 
-// refs.searchForm.addEventListener('submit', onSearch);
-// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+function onSearch(e) {
+  e.preventDefault();
 
-// function onSearch(e) {
-//   e.preventDefault();
+  newsApiService.query = e.currentTarget.elements.query.value;
 
-//   newsApiService.query = e.currentTarget.elements.query.value;
+  if (newsApiService.query === '') {
+    return alert('Введи что-то нормальное');
+  }
 
-//   if (newsApiService.query === '') {
-//     return alert('Введи что-то нормальное');
-//   }
+  loadMoreBtn.show();
+  newsApiService.resetPage();
+  clearArticlesContainer();
+  fetchArticles();
+}
 
-//   loadMoreBtn.show();
-//   newsApiService.resetPage();
-//   clearArticlesContainer();
-//   fetchArticles();
-// }
+function fetchArticles() {
+  loadMoreBtn.disable();
+  newsApiService.fetchArticles().then(articles => {
+    appendArticlesMarkup(articles);
+    loadMoreBtn.enable();
+  });
+}
 
-// function fetchArticles() {
-//   loadMoreBtn.disable();
-//   newsApiService.fetchArticles().then(articles => {
-//     appendArticlesMarkup(articles);
-//     loadMoreBtn.enable();
-//   });
-// }
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
 
-// function appendArticlesMarkup(articles) {
-//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
-// }
-
-// function clearArticlesContainer() {
-//   refs.articlesContainer.innerHTML = '';
-// }
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
