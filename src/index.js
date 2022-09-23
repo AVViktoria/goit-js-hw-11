58862037008a097e81f9142062d22574d3d013be
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-22 22:02:57 -0700

    dvdv

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Notify } from 'notiflix';
 
-import axios from 'axios';
-
 //*   IMPORTS  components  files
-import templateFunction from './templates/oneCardTemplate.hbs';
-// document.body.innerHTML = templateFunction();
-import { getImgs } from './js/fetchApi';
 import './css/common.css';
+import cardTemplate from './templates/oneCardTemplate.hbs';
+// document.body.innerHTML = templateFunction();
+import fetchImages from './js/fetchApi';
+
 // import NewsApiService from './js/news-service';
 import { onClickLoadMoreBtn } from './js/load-more-btn';
 
@@ -30,7 +29,7 @@ const refs = {
 
 
 function renderCardImage(arr) {
-  const markup = arr.map(item => templateFunction(item)).join('');
+  const markup = arr.map(item => cardTemplate(item)).join('');
   gallery.insertAdjacentHTML('beforeend', markup);
 }
 
@@ -45,7 +44,7 @@ let searchQuery = '';
 let currentHits = 0;
 
 refs.searchInput.addEventListener('submit', onSearch);
-loadMoreBtn.refs.button.addEventListener('click', onClickLoadMoreBtn);
+refs.button.addEventListener('click', onClickLoadMoreBtn);
 
 const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
@@ -59,31 +58,67 @@ function onSearch(e) {
   e.preventDefault();
 
   // newsApiService.query = e.currentTarget.elements.query.value;
- searchQuery = e.currentTarget.elements.query.value;
- refs.gallery.innerHTML = '';
- currentPage = 1;
- if (searchQuery === '') {
-  return Notiflix.Notify.failure("Sorry, you didn't write anything");
+  searchQuery = e.currentTarget.elements.query.value;
+  // refs.gallery.innerHTML = '';
+  currentPage = 1;
+  if (searchQuery === '') {
+    return Notiflix.Notify.failure("Sorry, you didn't write anything");
+  }
+  const response = await fetchImages(searchQuery, currentPage);
+  currentHits = response.hits.length;
+
+  if (response.totalHits > 40) {
+    loadMoreBtn.classList.remove('is-hidden');
+  } else {
+    loadMoreBtn.classList.add('is-hidden');
+  }
+
+  try {
+    if (response.totalHits > 0) {
+      Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      gallery.innerHTML = '';
+      renderCardImage(response.hits);
+      lightbox.refresh();
+      endCollectionText.classList.add('is-hidden');
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * -100,
+        behavior: 'smooth',
+      });
+    }
+
+    if (response.totalHits === 0) {
+      gallery.innerHTML = '';
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      loadMoreBtn.classList.add('is-hidden');
+      endCollectionText.classList.add('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+  //   loadMoreBtn.show();
+  //   newsApiService.resetPage();
+  //   clearArticlesContainer();
+  //   fetchArticles();
+  // }
 
-//   loadMoreBtn.show();
-//   newsApiService.resetPage();
-//   clearArticlesContainer();
-//   fetchArticles();
-// }
+  // function fetchArticles() {
+  //   loadMoreBtn.disable();
+  //   newsApiService.fetchArticles().then(articles => {
+  //     appendArticlesMarkup(articles);
+  //     loadMoreBtn.enable();
+  //   });
+  // }
 
-// function fetchArticles() {
-//   loadMoreBtn.disable();
-//   newsApiService.fetchArticles().then(articles => {
-//     appendArticlesMarkup(articles);
-//     loadMoreBtn.enable();
-//   });
-// }
+  // function appendArticlesMarkup(articles) {
+  //   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+  // }
 
-// function appendArticlesMarkup(articles) {
-//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
-// }
+  // function clearArticlesContainer() {
+  //   refs.articlesContainer.innerHTML = '';
 
-// function clearArticlesContainer() {
-//   refs.articlesContainer.innerHTML = '';
-}
