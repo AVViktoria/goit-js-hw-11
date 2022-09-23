5da5480b29fe3516d99065bcbbe12a0529ba844a
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-09-22 18:55:19 -0700

    did components files, refs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//*   IMPORTS  libraries
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
@@ -5,5 +7,60 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Notify } from 'notiflix';
 
 import axios from 'axios';
-// import templateFunction from './templates/oneCardTemplate.hbs';
-// document.body.innerHTML = templateFunction();
+
+
+//*   IMPORTS  components  files
+import templateFunction from './templates/oneCardTemplate.hbs';
+document.body.innerHTML = templateFunction();
+
+import "./css/common.css";
+import NewsApiService from './js/news-service';
+import LoadMoreBtn from './js/load-more-btn';
+
+//*     CONSTANTS
+// const refs = {
+//   searchForm: document.querySelector('.search-form'),
+//   searchInput: document.querySelector('[type="text"]'),
+//   btn: document.querySelector('[type="submit"]'),
+// };
+
+
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '[data-action="load-more"]',
+//   hidden: true,
+// });
+// const newsApiService = new NewsApiService();
+
+// refs.searchForm.addEventListener('submit', onSearch);
+// loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   newsApiService.query = e.currentTarget.elements.query.value;
+
+//   if (newsApiService.query === '') {
+//     return alert('Введи что-то нормальное');
+//   }
+
+//   loadMoreBtn.show();
+//   newsApiService.resetPage();
+//   clearArticlesContainer();
+//   fetchArticles();
+// }
+
+// function fetchArticles() {
+//   loadMoreBtn.disable();
+//   newsApiService.fetchArticles().then(articles => {
+//     appendArticlesMarkup(articles);
+//     loadMoreBtn.enable();
+//   });
+// }
+
+// function appendArticlesMarkup(articles) {
+//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+// }
+
+// function clearArticlesContainer() {
+//   refs.articlesContainer.innerHTML = '';
+// }
---
+++ src/js/news-service.js
@@ -0,0 +1,41 @@
+// const API_KEY = '30114983-364137b9a9ec33f130a531f95';
+// const BASE_URL = 'https://pixabay.com/api/';
+// const options = {
+//   headers: {
+//     Authorization: API_KEY,
+//   },
+// };
+
+// export default class NewsApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
+
+//   fetchArticles() {
+//     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+//     return fetch(url, options)
+//       .then(response => response.json())
+//       .then(({ articles }) => {
+//         this.incrementPage();
+//         return articles;
+//       });
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
